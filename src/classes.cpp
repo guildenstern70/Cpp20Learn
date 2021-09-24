6bdb5de018e38d753cc9984b2e617cb386293341
Alessio Saltarin <alessiosaltarin@gmail.com>
2021-09-24 15:26:58 +0200

    Classes enhancement

--- src/classes.cpp
+++ src/classes.cpp
@@ -8,13 +8,6 @@
 #include "classes.h"
 #include "utilities.h"
 
-Box::Box(double length, double width, double height)
-{
-    this->length = length;
-    this->height = height;
-    this->width = width;
-}
-
 double Box::volume() const
 {
     return this->width * this->height * this->length;
@@ -23,11 +16,23 @@ double Box::volume() const
 void testbox()
 {
     printheader("      CLASSES ");
-    std::cout << "Box volume is " << boxvolume(12.0, 21.3, 13.4) << std::endl;
+    auto box = Box { 10 };
+    auto anotherBox = Box { box };
+    anotherBox.setHeight(10.2);
+    anotherBox.setWidth(21.1);
+    anotherBox.setLength(1.21);
+    std::cout << "Box 1 volume is " << boxvolume(12.0, 21.3, 13.4) << std::endl;
+    std::cout << "Box 2 volume is " << boxvolume( box ) << std::endl;
+    std::cout << "Box 3 volume is " << boxvolume( anotherBox ) << std::endl;
 }
 
 double boxvolume(double length, double width, double height)
 {
     Box abox { length, width, height };
     return abox.volume();
+}
+
+double boxvolume(Box box)
+{
+    return box.volume();
 }
--- src/classes.h
+++ src/classes.h
@@ -14,10 +14,48 @@ class Box
 {
 public:
 
-    Box(double length, double width, double height);
+    Box() = default;
+
+    // Constructor with member initializer list
+    // This is preferrable to initialize members
+    // in function's body.
+    Box(double length, double width, double height) :
+            length { length }, width { width }, height { height } {};
+
+    // Copy constructor: construct box from another box.
+    // You should never define one, because the compiler already
+    // produce one for you.
+    // Same as
+    // Box(const Box& box)
+    //     length { box.length }, width { box.width }, height { box.height } {};
+    // To prohibit copy construction:
+    //     Box(const Box& box) = delete;
+    Box(const Box& box) = default;
+
+    // Constructor with a single parameter
+    // can be called for implicit conversions. To prevent this
+    // use 'explicit' keywork
+    explicit Box(double length) :
+        length { length }
+    {
+        this->height = 1;
+        this->width = 1;
+    }
 
     // nodiscard means that this return value SHOULD NOT be ignored
     [[nodiscard]] double volume() const;
+
+    // Getters
+    // Const keyword means that this method do not change the object state.
+    // This is 'const correctness'
+    [[nodiscard]] double getLength() const { return this->length; }
+    [[nodiscard]] double getWidth() const { return this->width; }
+    [[nodiscard]] double getHeight() const { return this->height; }
+
+    // Setters
+    void setLength(double aLength) { this->length = aLength; }
+    void setWidth(double aWidth) { this->length = aWidth; }
+    void setHeight(double aHeight) { this->length = aHeight; }
 
 private:
     double length { 1.0 };
@@ -27,6 +65,7 @@ private:
 };
 
 void testbox();
+double boxvolume(Box box);
 double boxvolume(double length, double width, double height);
 
 
