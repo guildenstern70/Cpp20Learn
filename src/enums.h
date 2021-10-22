b2666f31690ca6ff34aebc0bd6e07836a6ab6419
Guildenstern70 <alessiosaltarin@gmail.com>
2021-10-22 16:47:05 +0200

    Added enums

---
+++ src/enums.h
@@ -0,0 +1,20 @@
+//
+// Created by aless on 22/10/2021.
+//
+
+#ifndef CPP20LEARN_ENUMS_H
+#define CPP20LEARN_ENUMS_H
+
+enum class Color
+{
+    WHITE,
+    BLACK,
+    BLUE,
+    YELLOW,
+    GREEN,
+    GRAY,
+    ORANGE,
+    RED
+};
+
+#endif //CPP20LEARN_ENUMS_H
