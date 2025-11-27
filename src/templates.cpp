54e82a628e18f2b4f27aa9839d3c2bbf20865755
Alessio Saltarin <alessiosaltarin@gmail.com>
2025-11-27 14:59:32 +0100

    Template clarification

--- src/templates.cpp
+++ src/templates.cpp
@@ -15,7 +15,12 @@ T  get_larger_between(T a, T b)
     return a > b ? a : b;
 }
 
-double tell_me_which_is_larger(double a, double  b)
+double tell_me_which_is_larger(const double a, const double  b)
+{
+    return get_larger_between(a, b);
+}
+
+int tell_me_which_is_larger(const int a, const int  b)
 {
     return get_larger_between(a, b);
 }
@@ -23,4 +28,5 @@ double tell_me_which_is_larger(double a, double  b)
 void which_is_larger()
 {
     std::cout << "The larger between 45.0 and 67.9 is " << tell_me_which_is_larger(45.0, 67.9)  << std::endl;
+    std::cout << "The larger between 60 and 40 is " << tell_me_which_is_larger(60, 40)  << std::endl;
 }
